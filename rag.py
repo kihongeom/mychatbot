aa5352cf8e78b940287634f82965c72d8f5d9562
Kihong Eom <kheom@knu.ac.kr>
2024-08-19 10:45:23 +0900

    Add files via upload

--- rag.py
+++ rag.py
@@ -224,22 +224,22 @@ def download_auto_subtitles(youtube_url, language='ko'):
         'writesubtitles': True,  # 자막을 다운로드
         'subtitleslangs': [language],  # 자막 언어 설정
         'subtitlesformat': 'vtt',  # 자막 파일 형식을 .vtt로 지정
+        'format': 'best',
         'quiet': True, # Suppress output
         'no_warnings': True, # Suppress warnings
         'restrictfilenames': True,  # Avoid potential filename issues
-        'writesubtitles': True,  # Attempt to download subtitles
-        'logger':  Logger(),  # Use the custom logger,
+        'writesubtitles': True,  # Attempt to download subtitles 
         'skip_download': True,  # 비디오 파일은 다운로드하지 않음
         'outtmpl': '%(title)s.%(ext)s',  # 저장할 파일명 형식 지정
-        'writeautomaticsub': True  # 자동 생성된 자막 다운로드
+        'writeautomaticsub': True,  # 자동 생성된 자막 다운로드
+        'nocache': True,  # yt-dlp 캐시 사용 안함
     }
 
     # yt-dlp를 사용해 YouTube 정보를 추출하고 자막을 다운로드
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        
         try:
-            info_dict = ydl.extract_info(youtube_url)
-
+            # ydl.download([youtube_url])
+            ydl.extract_info(youtube_url)
         except yt_dlp.utils.DownloadError as e:
             print(f"Download error: {str(e)}")
         
